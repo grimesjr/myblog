a9f39933dcad5e4a2fbe6b9bd8f12daab8ed411e
Jimmy Lin <jimmylin@Lillians-MacBook-Pro-2018.local>
2020-05-28 01:07:21 -0700

    responsive menu

--- src/App.js
+++ src/App.js
@@ -10,10 +10,15 @@ import {
 import PageRenderer from './page-renderer';
 
 function App() {
+  const user = {
+    firstName: 'Jimmy',
+    lastName: 'Lin',
+  };
+
   return (
     <Router>
       <div className="App">
-        <Navigation />
+        <Navigation user={user} />
         <Switch>
           <Route path="/:page" component={PageRenderer} />
           <Route path="/" render={() => <Redirect to="/home" />} />
