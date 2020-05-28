c361eb5254b4abe5c2514261d4862131dbe6168b
Jimmy Lin <jimmylin@Lillians-MacBook-Pro-2018.local>
2020-05-27 17:45:49 -0700

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Navigation from './components/navigation';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
